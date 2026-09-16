7149e7130969c62ed467eefe4b40a47b8da0daa8
Bugsters104 <pasts@pasts.lv>
2026-09-16 13:52:23 +0300

    Atrod mazāko vērtību no visiem metieniem

--- src/git1/Kaulins.java
+++ src/git1/Kaulins.java
@@ -4,14 +4,19 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Kaulins {
-	static void mestKaulinu(int reizes) {
+	static int mestKaulinu(int reizes) {
 		int skaitlis;
 		Random rand = new Random();
-
+		int min = 6;
+		
 		for(int i=1; i<=reizes; i++) {
 			skaitlis = rand.nextInt(6)+1;
 			System.out.println("Uzkrita skaitlis: "+skaitlis);
+			if(skaitlis < min)
+				min = skaitlis;
 		}
+		
+		return min;
 	}
 	
 	public static void main(String[] args) {
@@ -19,7 +24,8 @@ public class Kaulins {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Cik reizes mest kauliņu?");
 		reizes = scan.nextInt();
-		mestKaulinu(reizes);
+		System.out.println("Mazākais skaitlis no visiem metieniem: "
+				+ mestKaulinu(reizes));
 
 		scan.close();
 	}
